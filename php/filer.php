70cdd2e00a25690be81f40871a4045cbf5d682d9
Mat1th <matthias@blackbox.local>
2016-11-05 18:20:22 +0100

    add connection to DB

--- php/filer.php
+++ php/filer.php
@@ -5,37 +5,37 @@
 </head>
 <body>
 <?php
-define("UPLOAD_DIR", "/var/www/html/upload/");
- 
-if (!empty($_FILES["myFile"])) {
-    $myFile = $_FILES["myFile"];
- 
-    if ($myFile["error"] !== UPLOAD_ERR_OK) {
-        echo "<p>An error occurred.</p>";
+define('UPLOAD_DIR', '/var/www/html/upload/');
+
+if (!empty($_FILES['myFile'])) {
+    $myFile = $_FILES['myFile'];
+
+    if ($myFile['error'] !== UPLOAD_ERR_OK) {
+        echo '<p>An error occurred.</p>';
         exit;
     }
- 
+
     // ensure a safe filename
-    #$name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
-	$name = "dat.csv";
-    #// don't overwrite an existing file
-    #$i = 0;
-    #$parts = pathinfo($name);
-    #while (file_exists(UPLOAD_DIR . $name)) {
-     #   $i++;
-     #   $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
-   # }
- 
+    //$name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
+    $name = 'dat.csv';
+    //// don't overwrite an existing file
+    //$i = 0;
+    //$parts = pathinfo($name);
+    //while (file_exists(UPLOAD_DIR . $name)) {
+     //   $i++;
+     //   $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+   // }
+
     // preserve file from temporary directory
-    $success = move_uploaded_file($myFile["tmp_name"],
-        UPLOAD_DIR . $name);
-    if (!$success) { 
-        echo "<p>Unable to save file.</p>";
+    $success = move_uploaded_file($myFile['tmp_name'],
+        UPLOAD_DIR.$name);
+    if (!$success) {
+        echo '<p>Unable to save file.</p>';
         exit;
     }
- 
+
     // set proper permissions on the new file
-    chmod(UPLOAD_DIR . $name, 0644);
+    chmod(UPLOAD_DIR.$name, 0644);
 }
 ?>
 <img src='spin.gif'/>
