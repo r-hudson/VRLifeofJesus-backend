0aa0f4b7329435db27f7d4f6261a035743f31dee
Rachel Hudson <r.hudson@bids.ca>
2016-11-05 18:54:39 +0100

    form to add clip

---
+++ php/get_content.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <title>Upload Clip</title>
+</head>
+
+<body>
+
+
+
+<form>
+
+    <h2>General</h2>
+
+    Name:
+    <input type="text" name = "name">
+    <br><br>
+    </input>
+
+    Description:
+    <input type="text" name = "description">
+    <br><br>
+
+    Timeline Position:
+    <input type="range" name = "timeline">
+    <br><br>
+
+    <h2>File(s)</h2>
+
+
+   <!--
+   Modify slightly for file upload function
+
+   Thumbnail:
+    <input onclick="addFile()";  type="button" value= "Add Thumbnail">
+    <br>
+
+    Video:
+    <input onclick="addFile()";  type="button" value= "Add File">
+    <br>
+    -->
+
+
+</form>
+
+</body>
+</html>
